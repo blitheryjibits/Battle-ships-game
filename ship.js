bdb3f777e74cf43380f812d88eab1459b5571d04
robert thornton <robert.thornton92@gmail.com>
2024-08-22 13:47:11 +0300

    Added getters and setters as well as additional properties such as orientation of the ship. Removed redundant singular x, y properties.

--- ship.js
+++ ship.js
@@ -3,27 +3,30 @@ class Ship {
     constructor(name, length,x1, x2, y1, y2) {
         this.name = name || "boat";
         this.length = length;
-        this.xCoords = this.setCoords(Math.min(x1,x2), Math.max(x1,x2));
-        this.yCoords = this.setCoords(Math.min(y1,y2), Math.max(y1,y2));
-        // this.x1 = x1;
-        // this.x2 = x2;
-        // this.y1 = y1;
-        // this.y2 = y2;
+        this.xCoords = x1 && x2 ? this.setCoords(Math.min(x1,x2), Math.max(x1,x2)) : null;
+        this.yCoords = y1 && y2 ? this.setCoords(Math.min(y1,y2), Math.max(y1,y2)) : null;
         this.hits = 0;
         this.sunk = false;
-
+        this.orientation = 0;
     }
     
     hit = () => { 
         this.hits++;
         this.length === this.hits ? this.sunk = true : this.sunk; 
     }
-    isSunk = () => { return this.sunk }
+    isSunk = () => { return this.sunk; }
+    getxCoords = () => { return this.xCoords; }
+    getyCoords = () => { return this.yCoords; }
+    getLength = () => { return this.length; }
+    getName = () => { return this.name; }
+    getOrientation = () => { return this.orientation; }
+
     setCoords = (coord1, coord2) => {
         return Array.from({length: coord2-coord1 +1}, (_, i) => {return coord1+i});
     }
-    getxCoords = () => { return this.xCoords; }
-    getyCoords = () => { return this.yCoords; }
+    setxCoords = (x, shipLength) => { this.xCoords = setCoords(x, x+shipLength); }
+    setyCoords = (y, shipLength) => { this.yCoords = setCoords(y, y+shipLength); }
+    setOrientation = (angle) => { this.orientation = angle; }
 }
 
-module.exports = Ship;
+export default Ship;
