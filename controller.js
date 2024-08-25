cf5a18e931573a9b4e6b37256dfc5c44d5d1d2c3
robert thornton <robert.thornton92@gmail.com>
2024-08-25 15:09:50 +0300

    Added eventlisteners from UI module to the controller so data can be easily passed between modules. Namely, Ship and Player objects can be passed into the event occurences to ensure their properties are updated when an event happens, i.e., a click event that represents an attack will be passed to a ship object and the gameboard object through the Player object.

--- controller.js
+++ controller.js
@@ -3,15 +3,42 @@ import Player from '/player.js';
 import Ship from './ship.js';
 import Gameboard from './gameboard.js'
 
-const grid = document.getElementById('game-grid');
-const ships = document.getElementById('ships');
+const player1Grid = document.querySelector('.game-grid.player1');
+const player2Grid = document.querySelector('.game-grid.player2');
+const player1ships = document.querySelector('.ships.player1');
+const player2ships = document.querySelector('.ships.player2');
 const gridWidth = 10;
+        // const shipList = [
+        //     {name:'patrol boat', length: 2}, 
+        //     {name:'submarine', length: 3},
+        //     {name:'destroyer', length: 3},
+        //     {name:'battleship', length: 4},
+        //     {name:'carrier', length: 5} 
+        // ];
 
-// const player = new Player('user', new Gameboard())
+const player1 = new Player('player1', new Gameboard());
+const player2 = new Player('player2', new Gameboard());
 
-BattleshipUI.init(grid, ships, gridWidth);
+const player1UI = new BattleshipUI(player1, Ship);
+const player2UI = BattleshipUI(player2, Ship);
+player1UI.init(player1Grid, player1ships, gridWidth);
+player2UI.init(player2Grid, player2ships, gridWidth);
 
-BattleshipUI.createGrid();
+player1UI.createGrid();
+player2UI.createGrid();
 
-// console.log(player);
+player1UI.grid.addEventListener('dragover', player1UI.handleDragOver);
+player1UI.grid.addEventListener('drop', player1UI.handleDrop.bind(player1UI));
 
+player1UI.shipsArray.forEach(ship => {
+        ship.addEventListener('dragstart', player1UI.handleDragStart);
+        ship.addEventListener('dblclick', player1UI.rotate);
+    });
+
+player2UI.grid.addEventListener('dragover', player2UI.handleDragOver);
+player2UI.grid.addEventListener('drop', player2UI.handleDrop.bind(player2UI));
+    
+player2UI.shipsArray.forEach(ship => {
+            ship.addEventListener('dragstart', player2UI.handleDragStart);
+            ship.addEventListener('dblclick', player2UI.rotate);
+        });
