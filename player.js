158106cc7e7d49b7889641b1f98e08fe319b7345
robert thornton <robert.thornton92@gmail.com>
2024-08-30 14:41:49 +0300

    Removed trial code for a random ship generator.

--- player.js
+++ player.js
@@ -9,26 +9,9 @@ export default class Player {
 }
 
 export class NPC extends Player {
-    constructor(name, gameboard, shipList) {
+    constructor(name, gameboard) {
         super(name, gameboard);
-        this.shipList = shipList;
-    }
 
-    randomCoordGenerator() {
-        return [Math.random()*10, Math.random()*10];
-    }
-    populateBoard() {
-        for (ship of shipList) {
-            startIndex = this.randomCoordGenerator();
-            if (ship.getOrientation === 0) {
-                ship.setxCoords(strartIndex[0], strartIndex[0]);
-                ship.setyCoords(startIndex[1], ship.getLength());
-            } else {
-                ship.setxCoords(strartIndex[0], ship.getLength());
-                ship.setyCoords(startIndex[1], startIndex[1]);
-            }
-            this.gb.addShip(...ship);
-        }
     }
     
 }
